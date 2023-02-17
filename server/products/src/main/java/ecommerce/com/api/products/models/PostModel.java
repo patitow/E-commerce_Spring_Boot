d607adda521917f518017930ba157e902b4c12de
patitow <msoliver2002@gmail.com>
2023-02-17 12:12:23 -0300

    modelo de validação e listar produtos

--- server/products/src/main/java/ecommerce/com/api/products/models/PostModel.java
+++ server/products/src/main/java/ecommerce/com/api/products/models/PostModel.java
@@ -1,5 +1,15 @@
 package ecommerce.com.api.products.models;
 
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// classe responsável por filtrar e validar requests
+@Component
+@Getter
+@Setter
 public class PostModel {
     
+    private String message;
 }
--- server/products/src/main/java/ecommerce/com/api/products/service/ProductService.java
+++ server/products/src/main/java/ecommerce/com/api/products/service/ProductService.java
@@ -1,5 +1,20 @@
 package ecommerce.com.api.products.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ecommerce.com.api.products.models.ProductModel;
+import ecommerce.com.api.products.repo.ProductRepo;
+
+@Service
 public class ProductService {
     
+    @Autowired
+    private ProductRepo pr;
+     
+    //Método para listar todos os produtos
+    public Iterable<ProductModel> listarProdutos(){
+        return pr.findAll();
+    }
+
 }

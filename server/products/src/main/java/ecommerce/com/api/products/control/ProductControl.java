886023b05dc523bc23fe0b14936523717a2b93a1
patitow <msoliver2002@gmail.com>
2023-02-17 13:38:04 -0300

    rotas, metodos e respostas a metodos http

--- server/products/src/main/java/ecommerce/com/api/products/control/ProductControl.java
+++ server/products/src/main/java/ecommerce/com/api/products/control/ProductControl.java
@@ -1,11 +1,39 @@
 package ecommerce.com.api.products.control;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ecommerce.com.api.products.models.ProductModel;
+import ecommerce.com.api.products.service.ProductService;
 
 @RestController
 public class ProductControl {
     
+    // implementa as rotas
+
+    @Autowired
+    private ProductService ps;
+
+    @GetMapping("/produtos")
+    public Iterable<ProductModel> listar(){
+        return ps.listarProdutos();
+    }
+
+    @GetMapping("/moletons")
+    public Iterable<ProductModel> listarTipo1(){
+        return ps.listarProdutos();
+    }
+    
+    @GetMapping("/camisetas")
+    public Iterable<ProductModel> listarTipo2(){
+        return ps.listarProdutos();
+    }
+
+    @GetMapping("/regatas")
+    public Iterable<ProductModel> listarTipo3(){
+        return ps.listarProdutos();
+    }
 
     @GetMapping("/")
     public String route(){
--- server/products/src/main/java/ecommerce/com/api/products/service/ProductService.java
+++ server/products/src/main/java/ecommerce/com/api/products/service/ProductService.java
@@ -1,8 +1,11 @@
 package ecommerce.com.api.products.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import ecommerce.com.api.products.models.PostModel;
 import ecommerce.com.api.products.models.ProductModel;
 import ecommerce.com.api.products.repo.ProductRepo;
 
@@ -12,9 +15,34 @@ public class ProductService {
     @Autowired
     private ProductRepo pr;
      
+    @Autowired
+    private PostModel rm;
+
     //Método para listar todos os produtos
     public Iterable<ProductModel> listarProdutos(){
         return pr.findAll();
+    }
+    
+    //Método para cadastrar produtos
+    public ResponseEntity<?> cadastrar(ProductModel pm){
+        if(pm.getNomeProd()==""){
+            rm.setMessage("O nome do produto é obrigatório. \n");
+            return new ResponseEntity<PostModel>(rm, HttpStatus.BAD_REQUEST);
+        }
+        if(pm.getTipo()==""){
+            rm.setMessage("O tipo do produto é obrigatório. \n");
+            return new ResponseEntity<PostModel>(rm, HttpStatus.BAD_REQUEST);
+        }
+        if(pm.getMarca()==""){
+            rm.setMessage("A marca do produto é obrigatória. \n");
+            return new ResponseEntity<PostModel>(rm, HttpStatus.BAD_REQUEST);
+        }
+        if(pm.getPreco().toString()==""){
+            rm.setMessage("O preço do produto é obrigatório. \n");
+            return new ResponseEntity<PostModel>(rm, HttpStatus.BAD_REQUEST);
+        }
+        
+        return new ResponseEntity<ProductModel>(pr.save(pm),HttpStatus.CREATED);
     }
 
 }

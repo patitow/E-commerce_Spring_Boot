1c540f111a39893ca005707319dc806e938baa9f
patitow <msoliver2002@gmail.com>
2023-02-17 10:57:58 -0300

    criacao e edicao da table de produtos

---
+++ server/products/src/main/java/ecommerce/com/api/products/models/ProductModel.java
@@ -0,0 +1,27 @@
+package ecommerce.com.api.products.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+// esse arquivo deve gerar uma tabela
+
+@Entity
+@Table(name="Produtos")
+@Getter
+@Setter
+
+public class ProductModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomeProd;
+    private String marca;
+    private Float preco;
+    private String[] tamanhos;
+}

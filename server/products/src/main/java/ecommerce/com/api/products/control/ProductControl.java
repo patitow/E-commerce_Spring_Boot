02932fc7b72b57621a242f396dac87fd29bf7353
patitow <msoliver2002@gmail.com>
2023-02-18 09:31:19 -0300

    cors para qualquer acesso

--- server/products/src/main/java/ecommerce/com/api/products/control/ProductControl.java
+++ server/products/src/main/java/ecommerce/com/api/products/control/ProductControl.java
@@ -2,6 +2,7 @@ package ecommerce.com.api.products.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import ecommerce.com.api.products.models.PostModel;
 import ecommerce.com.api.products.service.ProductService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProductControl {
     
     // implementa as rotas
